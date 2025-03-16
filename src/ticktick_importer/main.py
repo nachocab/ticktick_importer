e5d9f193e71074ab26b2b662f51e9f16f80368a5
Nacho Caballero <nachocab@gmail.com>
2025-03-16 10:50:36 +0100

    Implement title, due_date and description/content

--- src/ticktick_importer/main.py
+++ src/ticktick_importer/main.py
@@ -1,7 +1,135 @@
-def add_numbers(a, b):
-    """Add two numbers and return the result."""
-    return a + b
+from enum import StrEnum
+import re
+from typing import Optional
+from lxml import etree
+
+
+class TickTickTask(StrEnum):
+    FOLDER_NAME = "Folder Name"
+    LIST_NAME = "List Name"
+    TITLE = "Title"
+    KIND = "Kind"
+    TAGS = "Tags"
+    CONTENT = "Content"
+    IS_CHECK_LIST = "Is Check list"
+    START_DATE = "Start Date"
+    DUE_DATE = "Due Date"
+    REMINDER = "Reminder"
+    REPEAT = "Repeat"
+    PRIORITY = "Priority"
+    STATUS = "Status"
+    CREATED_TIME = "Created Time"
+    COMPLETED_TIME = "Completed Time"
+    ORDER = "Order"
+    TIMEZONE = "Timezone"
+    IS_ALL_DAY = "Is All Day"
+    IS_FLOATING = "Is Floating"
+    COLUMN_NAME = "Column Name"
+    COLUMN_ORDER = "Column Order"
+    VIEW_MODE = "View Mode"
+    TASK_ID = "taskId"
+    PARENT_ID = "parentId"
+
+
+DYNALIST_ITEM_PATTERN = re.compile(
+    r"\s*"  # Optional leading whitespace
+    r"(?:(\d+)\s+)?"  # Optional importance
+    r"(?:!\("  # Opening date and recurrence
+    r"(\d{4}-\d{2}-\d{2})\s*"  # date
+    r"(?:\|\s*"  # Optional recurrence
+    r"([^)]*)\s*)?"  # recurrence
+    r"\)\s*)?"  # Closing date and recurrence
+    r"(.*)"  # Title
+)
+
+
+class DynalistTask(StrEnum):
+    IMPORTANCE = "importance"
+    DUE_DATE = "due_date"
+    RECURRENCE = "recurrence"
+    TITLE = "title"
+    DESCRIPTION = "description"
+
+
+def get_dynalist_task(text: str) -> dict:
+    match = re.fullmatch(DYNALIST_ITEM_PATTERN, text)
+    if not match:
+        return {}
+
+    importance, due_date, recurrence, title = match.groups()
+    result = {}
+
+    if importance:
+        result[DynalistTask.IMPORTANCE] = int(importance)
+    if due_date:
+        result[DynalistTask.DUE_DATE] = f"{due_date}T23:00:00+0000"
+    if recurrence:
+        result[DynalistTask.RECURRENCE] = recurrence
+    if title:
+        result[DynalistTask.TITLE] = title
+
+    return result
+
+
+def extract_tasks_from_opml(file_path):
+    parser = etree.XMLParser(recover=True)
+    tree = etree.parse(file_path, parser)
+    root = tree.getroot()
+
+    tasks = []
+
+    def get_nesting_info(element, level=0):
+        text = element.get("text", "")
+        note = element.get("_note", "")
+
+        if text.startswith("!("):
+            # text = text.split(") ")[1]  # Remove the date and time from the title
+
+            dynalist_task = get_dynalist_task(text)
+
+            # parent = element.xpath("..")[0]  # Direct parent
+            # parent_text = parent.get("text", "None") if parent.tag == "outline" else "None"
+
+            task = {
+                TickTickTask.FOLDER_NAME: "",
+                TickTickTask.LIST_NAME: "",
+                TickTickTask.TITLE: dynalist_task.get(DynalistTask.TITLE, ""),
+                TickTickTask.KIND: "",
+                TickTickTask.TAGS: "",
+                TickTickTask.CONTENT: note,
+                TickTickTask.IS_CHECK_LIST: "",
+                TickTickTask.START_DATE: "",
+                TickTickTask.DUE_DATE: dynalist_task.get(DynalistTask.DUE_DATE, ""),
+                TickTickTask.REMINDER: "",
+                TickTickTask.REPEAT: "",
+                TickTickTask.PRIORITY: "",
+                TickTickTask.STATUS: "",
+                TickTickTask.CREATED_TIME: "",
+                TickTickTask.COMPLETED_TIME: "",
+                TickTickTask.ORDER: "",
+                TickTickTask.TIMEZONE: "",
+                TickTickTask.IS_ALL_DAY: "",
+                TickTickTask.IS_FLOATING: "",
+                TickTickTask.COLUMN_NAME: "",
+                TickTickTask.COLUMN_ORDER: "",
+                TickTickTask.VIEW_MODE: "",
+                TickTickTask.TASK_ID: "",
+                TickTickTask.PARENT_ID: "",
+            }
+            tasks.append(task)
+
+        for child in element:
+            get_nesting_info(child, level + 1)
+
+    get_nesting_info(root.xpath(".//outline")[0])
+
+    return tasks
 
 
 if __name__ == "__main__":
-    print(add_numbers(2, 3))
+    3
+    # file_path = "dynalist_export.opml"
+    # tasks = extract_tasks_from_opml(file_path)
+    # for task in tasks:
+    #     print(f"Task: {task['text']}")
+    #     print(f"Level: {task['level']}, Parent: {task['parent']}\n")
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,13 +1,90 @@
-from ticktick_importer.main import add_numbers
+from ticktick_importer.main import TickTickTask, extract_tasks_from_opml
 
 
-def test_add_numbers_positive():
-    assert add_numbers(2, 3) == 5
+def test_extract_tasks():
+    tasks = extract_tasks_from_opml("tests/data/sample.opml")
+    expected_tasks = [
+        {
+            TickTickTask.FOLDER_NAME: "",
+            TickTickTask.LIST_NAME: "",
+            TickTickTask.TITLE: "write a parser in python to import from dynalist to ticktick",
+            TickTickTask.KIND: "",
+            TickTickTask.TAGS: "",
+            TickTickTask.CONTENT: "",
+            TickTickTask.IS_CHECK_LIST: "",
+            TickTickTask.START_DATE: "",
+            TickTickTask.DUE_DATE: "2025-03-15T23:00:00+0000",
+            TickTickTask.REMINDER: "",
+            TickTickTask.REPEAT: "",
+            TickTickTask.PRIORITY: "",
+            TickTickTask.STATUS: "",
+            TickTickTask.CREATED_TIME: "",
+            TickTickTask.COMPLETED_TIME: "",
+            TickTickTask.ORDER: "",
+            TickTickTask.TIMEZONE: "",
+            TickTickTask.IS_ALL_DAY: "",
+            TickTickTask.IS_FLOATING: "",
+            TickTickTask.COLUMN_NAME: "",
+            TickTickTask.COLUMN_ORDER: "",
+            TickTickTask.VIEW_MODE: "",
+            TickTickTask.TASK_ID: "",
+            TickTickTask.PARENT_ID: "",
+        },
+        {
+            TickTickTask.FOLDER_NAME: "",
+            TickTickTask.LIST_NAME: "",
+            TickTickTask.TITLE: "arregla reposapiés taburete",
+            TickTickTask.KIND: "",
+            TickTickTask.TAGS: "",
+            TickTickTask.CONTENT: "asdf\nasdfd",
+            TickTickTask.IS_CHECK_LIST: "",
+            TickTickTask.START_DATE: "",
+            TickTickTask.DUE_DATE: "2025-03-12T23:00:00+0000",
+            TickTickTask.REMINDER: "",
+            TickTickTask.REPEAT: "",
+            TickTickTask.PRIORITY: "",
+            TickTickTask.STATUS: "",
+            TickTickTask.CREATED_TIME: "",
+            TickTickTask.COMPLETED_TIME: "",
+            TickTickTask.ORDER: "",
+            TickTickTask.TIMEZONE: "",
+            TickTickTask.IS_ALL_DAY: "",
+            TickTickTask.IS_FLOATING: "",
+            TickTickTask.COLUMN_NAME: "",
+            TickTickTask.COLUMN_ORDER: "",
+            TickTickTask.VIEW_MODE: "",
+            TickTickTask.TASK_ID: "",
+            TickTickTask.PARENT_ID: "",
+        },
+    ]
+
+    assert len(tasks) == len(expected_tasks)
+
+    for object, index in zip(tasks, range(len(tasks))):
+        assert object == expected_tasks[index]
 
 
-def test_add_numbers_negative():
-    assert add_numbers(-1, -1) == -2
-
-
-def test_add_numbers_zero():
-    assert add_numbers(0, 5) == 5
+# ( 1) "Folder Name" - "" 👍
+# ( 2) "List Name" - "📥Inbox task list" 👍
+# ( 3) "Title" - "high importance tarea for in " 👍
+# ( 4) "Kind" - "TEXT" 👍
+# ( 5) "Tags" - "my-tag" 👍
+# ( 6) "Content" - "with description" 👍
+# ( 7) "Is Check list" - "N" 👍
+# ( 8) "Start Date" - "2025-03-16T17:00:00+0000" 👍
+# ( 9) "Due Date" - "2025-03-16T17:00:00+0000"
+# (10) "Reminder" - "PT0S"
+# (11) "Repeat" - ""
+# (12) "Priority" - "5"
+# (13) "Status" - "0"
+# (14) "Created Time" - "2025-03-15T15:16:38+0000"
+# (15) "Completed Time" - ""
+# (16) "Order" - "-1099511627776"
+# (17) "Timezone" - "Europe/Madrid"
+# (18) "Is All Day" - "false"
+# (19) "Is Floating" - "false"
+# (20) "Column Name" -
+# (21) "Column Order" -
+# (22) "View Mode" - "list"
+# (23) "taskId" - "3"
+# (24) "parentId" - ""
