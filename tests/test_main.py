0ac0415913c1b23621c26eaea2a1f8b36fff2600
Nacho Caballero <nachocab@gmail.com>
2025-03-22 19:58:56 +0100

    remove WIP comments

--- tests/test_main.py
+++ tests/test_main.py
@@ -51,28 +51,3 @@ def test_extract_tasks():
         assert ticktick_task is not None
         assert ticktick_task.content == expected[index].content
 
-
-# ( 1) "Folder Name" - "" 👍
-# ( 2) "List Name" - "📥Inbox task list" 👍
-# ( 3) "Title" - "high importance tarea for in " 👍
-# ( 4) "Kind" - "TEXT" 👍
-# ( 5) "Tags" - "my-tag" 👍
-# ( 6) "Content" - "with description" 👍
-# ( 7) "Is Check list" - "N" 👍
-# ( 8) "Start Date" - "2025-03-16T17:00:00+0000" 👍
-# ( 9) "Due Date" - "2025-03-16T17:00:00+0000" 👍
-# (10) "Reminder" - "PT0S" 👍
-# (11) "Repeat" - "" 👍
-# (12) "Priority" - "5" 👍
-# (13) "Status" - "0" 👍
-# (14) "Created Time" - "2025-03-15T15:16:38+0000" 👍
-# (15) "Completed Time" - "" 👍
-# (16) "Order" - "-1099511627776" ???
-# (17) "Timezone" - "Europe/Madrid" 👍
-# (18) "Is All Day" - "false" 👍
-# (19) "Is Floating" - "false" 👍
-# (20) "Column Name" - 👍
-# (21) "Column Order" - 👍
-# (22) "View Mode" - "list" 👍
-# (23) "taskId" - "3" ???
-# (24) "parentId" - ""
